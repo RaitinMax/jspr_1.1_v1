c49c3391b9a687b2d72b3ee5a090d94812575ecc
Raitin PC <jhosh1212@gmail.com>
2022-08-07 22:46:30 +0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Main {
+    final static int PORT = 9999;
+    public static void main(String[] args) {
+        Server myServer = new Server(PORT);
+        myServer.toStart();
+    }
+}
+
+
